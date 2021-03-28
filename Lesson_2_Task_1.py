43a05be5390ad43d0a8d49b2977b6e72c54e268c
denjes_d <dolbnia.d@gmail.com>
2021-03-28 13:52:35 +0300

    test

---
+++ Lesson_2_Task_1.py
@@ -0,0 +1,9 @@
+
+apple_number = int(input('Enter the number of apples: '))
+students_number = int(input('Enter the number of students: '))
+
+apples_in_hands = apple_number // students_number
+rest_apples = apple_number % students_number
+
+print(f'Every student get {apples_in_hands} apples\n'
+      f'There are {rest_apples} apple left in the basket')
