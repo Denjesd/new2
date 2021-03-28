23ffa85c803dea5517dc915c73fd514efdb60faa
denjes_d <dolbnia.d@gmail.com>
2021-03-28 19:10:21 +0300

    Lesson 2 Task 2

--- Lesson 2/Lesson_2_Task_2.py
+++ Lesson 2/Lesson_2_Task_2.py
@@ -4,20 +4,9 @@ class_1 = int(input('Enter the number of studendts in class 1: '))
 class_2 = int(input('Enter the number of studendts in class 2: '))
 class_3 = int(input('Enter the number of studendts in class 3: '))
 
-if class_1 % 2 == 0:
-    desk_to_class_1 = class_1 // 2
-else:
-    desk_to_class_1 = class_1 // 2 + 1
-
-if class_2 % 2 == 0:
-    desk_to_class_2 = class_2 // 2
-else:
-    desk_to_class_2 = class_2 // 2 + 1
-
-if class_3 % 2 == 0:
-    desk_to_class_3 = class_3 // 2
-else:
-    desk_to_class_3 = class_3 // 2 + 1
+desk_to_class_1 = class_1 // 2 + class_1 % 2
+desk_to_class_2 = class_2 // 2 + class_2 % 2
+desk_to_class_3 = class_3 // 2 + class_3 % 2
 
 print(f'School has to buy {desk_to_class_1 + desk_to_class_2 + desk_to_class_3} desks.')
 
