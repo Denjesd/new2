c07778d7ec8a11dcd9242fb27f0cdcf30e4507b9
denjes_d <dolbnia.d@gmail.com>
2021-03-28 14:09:55 +0300

    Lesson 2 Task 1

--- Lesson 2/Lesson_2_Task_1.py
+++ Lesson 2/Lesson_2_Task_1.py
@@ -5,15 +5,14 @@
 apple_number = int(input('Enter the number of apples: '))
 students_number = int(input('Enter the number of students: '))
 if apple_number < students_number:
-      print('\nSorry, some student does not get any apple\n'
-            'Try to input another values\n')
       while apple_number < students_number:
-
+            print('\nSorry, some student does not get any apple\n'
+                  'Try to input another values\n')
             apple_number = int(input('Enter the number of apples: '))
             students_number = int(input('Enter the number of students: '))
 
 apples_in_hands = apple_number // students_number
 rest_apples = apple_number % students_number
 
-print(f'Every student get {apples_in_hands} apples\n'
+print(f'\nEvery student get {apples_in_hands} apples\n'
       f'There are {rest_apples} apple left in the basket')
