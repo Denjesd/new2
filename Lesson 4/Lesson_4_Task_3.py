61ef212efb90f270b02f3e27817af6560cc9d457
denjes_d <dolbnia.d@gmail.com>
2021-04-04 23:09:40 +0300

    Lesson 4 Task 3

---
+++ Lesson 4/Lesson_4_Task_3.py
@@ -0,0 +1,14 @@
+# Lesson 4 Task 3
+
+#input_string = input('Enter the string: ')
+
+input_string = 'jefv kowek pmefpqd iutjwdcv jekv a b c d prge'
+print(f'The third symbol is: \'{input_string[2]}\'.')
+print(f'Prelast symbol is: \'{input_string[-2]}\'.')
+print(f'The first five symbols are: \'{input_string[0:5]}\'.')
+print(f'String without two last symbols: \'{input_string[0:-2]}\'.')
+print(f'Even symbols: \'{input_string[1::2]}\'.')
+print(f'Odd symbols: \'{input_string[0::2]}\'.')
+print(f'Reversed string: \'{input_string[:: -1]}\'.')
+print(f'Reversed odd symbols from the last: \'{input_string[:: -2]}\'.')
+print(f'Length fo the string is {len(input_string)} symbols.')
