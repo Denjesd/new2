0e620469cbde0d46c847759b7982606358c8a340
denjes_d <dolbnia.d@gmail.com>
2021-04-13 00:22:53 +0300

    Lesson 6 Task 5

--- Lesson 6/Lesson_6_Task_5.py
+++ Lesson 6/Lesson_6_Task_5.py
@@ -1,5 +1,3 @@
 # Lesson 6 Task 5
 
-input_list_1 = [1, 1, 1, 2, 2, 2, 3, 3, 4]
-input_list_2 = [2, 2, 4, 5, 5, 6, 6, 8, 9, 9]
-print(f'Our set is: {set({elem for elem in input_list_1}) ^ set({item for item in input_list_2})}')
+print(f'Our set is: {set({elem for elem in [1, 1, 1, 2, 2, 2, 3, 3, 4]}) ^ set({item for item in [2, 2, 4, 5, 5, 6, 6, 8, 9, 9]})}')
