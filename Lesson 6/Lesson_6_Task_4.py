0c7333b93c5b2f5474383e12cc32910521b1a07d
denjes_d <dolbnia.d@gmail.com>
2021-04-13 00:19:55 +0300

    Lesson 6 Task 4

--- Lesson 6/Lesson_6_Task_4.py
+++ Lesson 6/Lesson_6_Task_4.py
@@ -1,4 +1,3 @@
 # Lesson 6 Task 4
 
-input_list = [1, 1, 1, 2, 2, 2, 3, 3, 4]
-print(f'Our set is: {set({elem for elem in input_list})}')
+print(f'Our set is: {set({elem for elem in [1, 1, 1, 2, 2, 2, 3, 3, 4]})}')
