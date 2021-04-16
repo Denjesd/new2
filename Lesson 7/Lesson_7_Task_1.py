be7684e0b09a0e838d9fedb9553c3dabe6050afd
denjes_d <dolbnia.d@gmail.com>
2021-04-16 23:31:36 +0300

    Lesson 7 Task 1

--- Lesson 7/Lesson_7_Task_1.py
+++ Lesson 7/Lesson_7_Task_1.py
@@ -1,25 +1,21 @@
 # Lesson 7 Task 1
+input_list = [
+                [34587, 'Learning Python, Mark Lutz', 4, 10.95],
+                [98762, 'Programming Python, Mark Lutz', 5, 56.80],
+                [77226, 'Head First Python, Paul Barry', 3, 32.95],
+                [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99],
+             ]
 
 print('Our new list is:', list(map(lambda item: (
                                     item[0],
-                                    (round(item[2] * item[3] ,2)
-                                        if round(item[2] * item[3] ,2) >= 100
+                                    (round(item[2] * item[3]
+                                        if item[2] * item[3] >= 100
                                         else
-                                            round(item[2] * item[3] ,2) + 10)),
-                                                        [
-                                                            [34587, 'Learning Python, Mark Lutz', 4, 10.95],
-                                                            [98762, 'Programming Python, Mark Lutz', 5, 56.80],
-                                                            [77226, 'Head First Python, Paul Barry', 3, 32.95],
-                                                            [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99],
-                                                        ]
+                                            item[2] * item[3] + 10, 2))),
+                                    input_list
                                     )
                                 )
       )
-
-# Lesson 7 Task 1
-
-
-
 
 
 
