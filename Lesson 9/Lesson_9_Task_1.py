883ee673a8f86ac68360045c019ec82c2d923a3f
denjes_d <dolbnia.d@gmail.com>
2021-04-23 22:45:35 +0300

    Lesson 9 Task 1

--- Lesson 9/Lesson_9_Task_1.py
+++ Lesson 9/Lesson_9_Task_1.py
@@ -1,6 +1,6 @@
 # Lesson 9 Task 1 calculator
 
-def calculate(num_a, num_b, operator):
+def arithmetic(num_a, num_b, operator):
 
     if operator == '+':
         return num_a + num_b
@@ -15,5 +15,5 @@ input_value_1 = int(input('Enter the first number: '))
 input_value_2 = int(input('Enter the second number: '))
 input_operator = input('Enter the opetaror (+, -, *, /) : ')
 
-print(f'Your result is: {calculate(input_value_1, input_value_2, input_operator)}')
+print(f'Your result is: {arithmetic(input_value_1, input_value_2, input_operator)}')
 
