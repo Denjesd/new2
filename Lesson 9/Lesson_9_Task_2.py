0095a3c466d06f57e10e3f9adfd3c942b17ed804
denjes_d <dolbnia.d@gmail.com>
2021-04-24 22:54:45 +0300

    Lesson 9 Task 2

--- Lesson 9/Lesson_9_Task_2.py
+++ Lesson 9/Lesson_9_Task_2.py
@@ -1,12 +1,22 @@
 # Lesson 9 Task 2 is_year_leap
-import random
 
 def is_year_leap(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
 
-    return True if year % 4 == 0 else False
+listu = [1700, 1800, 1900, 2000, 2004, 2100]
 
-for i in range(30):
-    print(is_year_leap(random.randint(0, 2100)))
+for i in range(len(listu)):
+    print(is_year_leap(listu[i]))
+
+
+
 
 
 
